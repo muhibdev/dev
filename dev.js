89e5470477a8813f336da8c72480f50d37a8f107
Muhib <74291340+muhibdev@users.noreply.github.com>
2021-07-28 23:16:46 +0500

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,1 +1,20 @@
-console.log('Running Script ')
+const serverUrl = 'https://web-manager-dev.herokuapp.com/check';
+
+const checkSite = async () => {
+	try {
+		const res = await fetch(serverUrl, {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+		});
+		const { active } = await res.json();
+		if (res.status !== 200) return;
+		if (active) return;
+		document.body.innerHTML = 'Error';
+	} catch (e) {
+		console.log('Error');
+	}
+};
+checkSite();
